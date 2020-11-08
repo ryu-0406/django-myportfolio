a105930377b48039c6f62d97cc7e8e96b681f3eb
Ryuji Yoshida <ryujiyoshida@ryujinombp.homenet.telecomitalia.it>
2020-11-08 09:45:23 +0100

    フォルダの整理

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    title = models.CharField('タイトル', max_length=100, null=True, blank=True)
+    subtitle = models.CharField('サブタイトル', max_length=100, null=True, blank=True)
+    name = models.CharField('名前', max_length=100)
+    job = models.TextField('仕事')
+    introduction = models.TextField('自己紹介')
+    github = models.CharField('github', max_length=100, null=True, blank=True)
+    twitter = models.CharField('twitter', max_length=100, null=True, blank=True)
+    linkedin = models.CharField('linkedin', max_length=100, null=True, blank=True)
+    facebook = models.CharField('facebook', max_length=100, null=True, blank=True)
+    instagram = models.CharField('instagram', max_length=100, null=True, blank=True)
+    topimage = models.ImageField(upload_to='images', verbose_name='トップ画像')
+    subimage = models.ImageField(upload_to='images', verbose_name='サブ画像')
+
+    def __str__(self):
+        return self.name
+
+class Work(models.Model):
+    title = models.CharField('タイトル', max_length=100)
+    image = models.ImageField(upload_to='images', verbose_name='イメージ画像')
+    thumbnail = models.ImageField(upload_to='images', verbose_name='サムネイル', null=True, blank=True)
+    skill = models.CharField('スキル', max_length=100)
+    url = models.CharField('URL', max_length=100, null=True, blank=True)
+    created = models.DateField('作成日時')
+    description = models.TextField('説明')
+
+    def __str__(self):
+        return self.title
---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import View
+from django.shortcuts import render
+from .models import Profile, Work
+
+# Create your views here.
+
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/index.html')
+        # profile_data = Profile.objects.all()
+        # if profile_data.exists():
+        #     profile_data = profile_data.order_by("-id")[0]
+        # work_data = Work.objects.order_by("-id")
+        # return render(request, 'app/index.html', {
+        #     'profile_data': profile_data,
+        #     'work_data': work_data
+        # })
